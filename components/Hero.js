7c0798178017f159d897df1b99a43d32013d2534
ben.koren <ben.koren@urban-dashboard.com>
2022-11-08 13:38:51 +0200

    increasing size for mobile

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ export default function Hero() {
                 <div className="w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
                     <RoughNotationGroup show={true}>
                         <RainbowHighlight color={colors[0]}>
-                            <h1 className="text-2xl md:text-7xl font-bold text-gray-700 dark:text-gray-200 ">
+                            <h1 className="text-4xl md:text-7xl font-bold text-gray-700 dark:text-gray-200 ">
                                 Engineer 🛠
                             </h1>
                         </RainbowHighlight>
